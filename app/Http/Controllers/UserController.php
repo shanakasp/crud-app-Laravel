0dcc8991cb9d6f081a716f3f12894953819972ba
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-14 21:08:34 +0530

    modified UserController.php in Controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -14,6 +15,9 @@ class UserController extends Controller
                 'password' => 'required|min:6'
             ] );
 
+$incomingFields['password'] = bcrypt($incomingFields['password']);
+
+            User::create($incomingFields);
         return 'Hello heloo';
     }
 }
